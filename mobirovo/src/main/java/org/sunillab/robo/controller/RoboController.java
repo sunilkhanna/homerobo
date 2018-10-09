a6dd392c7e3eaab32c0c4297093584446a9bf9de
id851738 <er.sunilkhanna@gmail.com>
2018-10-09 16:15:15 +0530

    FirstController

---
+++ mobirovo/src/main/java/org/sunillab/robo/controller/RoboController.java
@@ -0,0 +1,25 @@
+/***************************************************************************
+ *  Copyright (C) Belgacom 2018
+ *
+ *  The reproduction, transmission or use of this document  or its contents
+ *  is not  permitted without  prior express written consent of Belgacom.
+ *  Offenders will be liable for damages. All rights, including  but not
+ *  limited to rights created by patent grant or registration of a utility
+ *  model or design, are reserved.
+ *
+ *  Belgacom reserves the right to modify technical specifications and features.
+ *
+ *  Technical specifications and features are binding only insofar as they
+ *  are specifically and expressly agreed upon in a written contract.
+ *
+ **************************************************************************/
+
+package org.sunillab.robo.controller;
+
+/**
+ * @author sunil.khanna
+ *
+ */
+public class RoboController {
+
+}
