99879515d55fda592ffce6b06cd3a4987e9efd47
id851738 <er.sunilkhanna@gmail.com>
2018-10-10 15:25:59 +0530

    icc

---
+++ testgit/src/main/java/org/sunillab/controller/DirectionController.java
@@ -0,0 +1,33 @@
+/***************************************************************************
+ *  Copyright (C) Belgacom 2018
+ *
+ *  The reproduction, transmission or use of this document  or its contents
+ *  is not  permitted without  prior express written consent of Belgacom.
+ *  Offenders will be liable for damages. All rights, including  but not
+ *  limited to rights created by patent grant or registration of a utility
+ *  model or design, are reserved.
+ *
+ *  Belgacom reserves the right to modify technical specifications and features.
+ *
+ *  Technical specifications and features are binding only insofar as they
+ *  are specifically and expressly agreed upon in a written contract.
+ *
+ **************************************************************************/
+
+package org.sunillab.controller;
+
+/**
+ * @author sunil.khanna
+ *
+ */
+public class DirectionController {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Directed*****");
+
+	}
+
+}
